6defde7d973937a441d3960dacf47b9246129283
huuthinhntp <huuthinh817@gmail.com>
2022-07-18 00:47:05 +0700

    Buoi1

--- Buoi1/bai3.java
+++ Buoi1/bai3.java
@@ -6,28 +6,15 @@ public class bai3 {
         int n;
         System.out.println("Nhap n:");
         n = scanner.nextInt();
-        System.out.println("Tong cac so nguyen to la:"+tinhtong(n));
-    }
-    public static int tinhtong(int n)
-    {
         int tong=0;
-        for(int i=1;i<=n;i++)
+        for(int i=1 ;i<=n; i++)
         {
-            if(kiemtrasonguyento(i)==1)
+            if(n%i==0)
+            {
                 tong+=i;
+            }
         }
-        return tong;
+        System.out.println("Tong cac uoc cua n la:"+ tong);
     }
-    public static int kiemtrasonguyento(int n)
-    {
-        int dem=0;
-        for(int i=1; i<=n; i++) {
-            if (n % i == 0)
-                dem++;
-        }
-        if(dem==2)
-            return 1;
-        else
-            return 0;
-    }
+
 }
